7f1b823875dc1f6f92adab6709a734964c40c6f6
gkcoder-abs <goesta.kuklnski@abs-it-kiel.de>
2025-04-28 14:21:54 +0200

    Bachend.java  aenderung

--- Backend.java
+++ Backend.java
@@ -2,11 +2,7 @@
 public class Backend {
 	
 	public static void service() {
-<<<<<<< HEAD
-		System.out.println("Backend is running. Ready to receive inputs.");
-=======
 		System.out.println("Backend has started and is running.");
->>>>>>> 4c0aeec75cf53e7c8357ffffbd5a8dabd7872a77
 	}
 
 }
