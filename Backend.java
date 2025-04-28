e955a8ee11519628305fadcd997f71d309c0c2a5
Zyzyx420 <drufferus@gmail.com>
2025-04-28 14:04:14 +0200

    Fabis Aenderung.

--- Backend.java
+++ Backend.java
@@ -2,7 +2,7 @@
 public class Backend {
 	
 	public static void service() {
-		System.out.println("Backend is running.");
+		System.out.println("Backend is running. Ready to receive inputs.");
 	}
 
 }
